6bb81fe9ba24bda09c075a61807e21bd46c228b4
JoeBigToe <jrosa.main@gmail.com>
2021-12-22 10:14:02 +0100

    Day 16

--- day16/part1.py
+++ day16/part1.py
@@ -1,53 +1,63 @@
+from collections import deque
+from math import prod
+
 def read(path):
     with open(path) as fp:
         num = fp.read().strip()
         instruction = format(int(num, 16), f'0{4*len(num)}b')
-
     return instruction
 
-def read_literal(packet):
-    
-    group_size = 5
-    literal = ''
-    
-    blocks = [ packet[i:i + group_size] for i in range(0, len(packet), group_size) ]
-    
-    for block in blocks:
-        literal += block[1:]
-        if block[0] == '0':
-            break
-    
-    return literal
 
-def read_packet(instruction):
-    packet_version = int(instruction[:3], 2)
-    packet_type = int(instruction[3:6], 2)
+class Program:
 
-    if packet_type == 4:
-        packet = instruction[6:]
-        # return read_literal(packet)
-        return packet_version
-    else:
-        if instruction[6] == '0':
-            length = int(instruction[7:22],2)
-            tmp = instruction[22:22+length]
-            packets = []
-            for i in range(0, len(tmp), 11):
-                if i + 21 < len(tmp):
-                    packets.append(tmp[i:i + 11])
-                else:
-                    packets.append(tmp[i:])
+    def __init__(self, instruction):
+        self.instruction = deque(instruction)
+        self.version_sum = 0
+        self.bits_read = 0
+
+    def read(self, size):
+        self.bits_read += size
+        return "".join([self.instruction.popleft() for _ in range(size)])
+
+    def read_packet(self):
+        self.version_sum += int(self.read(3), 2)
+        packet_type = int(self.read(3), 2)
+
+        if packet_type == 4:
+            output = ''
+            while True:
+                stop =  self.read(1) == '0'                
+                output += self.read(4)
+                if stop:
+                    output = int(output, 2)
                     break
-
         else:
-            length = int(instruction[7:18],2)
-            tmp = instruction[18:18+11*length]
-            packets = [ tmp[i:i + 11] for i in range(0, len(tmp), 11) ]
+            packets = []
+            if self.read(1) == '0':                
+                length = int(self.read(15),2)
+                tmp = self.bits_read
+                while self.bits_read - tmp < length:
+                    packets.append(self.read_packet())
+            else:
+                c = int(self.read(11), 2)
+                for _ in range(c):
+                    packets.append(self.read_packet())
+
+            output = {
+                0: sum(packets),
+                1: prod(packets),
+                2: min(packets),
+                3: max(packets),
+                5: 1 if packets[0] > (packets[1] if len(packets) > 1 else 0) else 0 ,
+                6: 1 if packets[0] < (packets[1] if len(packets) > 1 else 0) else 0,
+                7: 1 if packets[0] == (packets[1] if len(packets) > 1 else 0) else 0 
+            }[packet_type]
         
-        for packet in packets:
-            packet_version += read_packet(packet)
-        
-        return packet_version
+        return output
 
 
-print(read_packet(read("./day16/puzzle.txt")))
+p = Program(read("./day16/puzzle.txt"))
+result = p.read_packet()
+
+print(f'Part 1: {p.version_sum}')
+print(f'Part 2: {result}')
